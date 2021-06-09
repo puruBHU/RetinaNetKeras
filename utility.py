1b21458421d4d3446eccebd3520449a92804ddcd
puruBHU <purnendu06@gmail.com>
2021-06-09 22:58:34 +0530

    added anchor generator

--- utility.py
+++ utility.py
@@ -1,5 +1,6 @@
+import numpy as np
 import tensorflow as tf
-
+from matplotlib import pyplot as plt
 
 def swap_xy(boxes):
 	'''
@@ -25,15 +26,66 @@ def convert_to_corner(boxes):
 	'''
 	return tf.concat([boxes[...,:2] - boxes[...,2:] / 2.0, boxes[...,:2] + boxes[...,2:] / 2.0], axis=-1)
 
+def compute_iou(boxes1, boxes2):
+	''':cvar
+	'''
+	boxes1_corners = convert_to_corner(boxes1)
+	boxes2_corners = convert_to_corner(boxes2)
+
+	lu = tf.maximum(boxes1_corners[:, None,:2], boxes2_corners[:,:2]) # left uppper (lu)
+	rd = tf.minimum(boxes1_corners[:, None,2:], boxes2_corners[:,2:]) # right down
+
+	intersection = tf.maximum(0.0, rd - lu)
+	intersection_area = intersection[...,0] * intersection[...,1]
+	boxes1_area = boxes1[:,2] * boxes1[:, 3]
+	boxes2_area = boxes2[:,2] * boxes2[:, 3]
+
+	union_area = tf.maximum(boxes1_area[:, None] + boxes2_area - intersection_area, 1e-8)
+
+	return tf.clip_by_value(intersection_area / union_area, 0.0, 1.0)
+
+def visualize_detection(image, boxes, classes, scores, figsize= (7,7),, linewidth=1, color=[0,0,1]):
+
+	image = np.array(image, dtype = np.uint8)
+	plt.figure(figsize = figsize)
+	plt.axis('off')
+	plt.imshow(image)
+	ax = plt.gca()
+	for box, _cls, score in zip(boxes, classes, scores):
+		text = "{}: {:.2f}".format(_cls, score)
+		x1, y1, x2, y2 = box
+		w, h = x2 - x1, y2 - y1
+		patch = plt.Rectangle([x1, y1], w,h, fill=False, edgecolor = color, linewidth=linewidth
+		                      )
+		ax.add_patch(patch)
+		ax.text(x1,
+		        y1,
+		        text,
+		        bbox={'facecolor':color, 'alpha':0.4},
+		        clip_box=ax.clipbox,
+		        clip_on=True
+		        )
+		plt.show()
+		return ax
+
 
 
 if __name__=='__main__':
-	box = tf.convert_to_tensor([0,0,2,2], dtype = tf.float32)
-	box = tf.reshape(box,(1,4))
+	box1 = tf.convert_to_tensor([0,0,2,2], dtype = tf.float32)
+	box2 = tf.convert_to_tensor([0,0, 3, 3], dtype = tf.float32)
+	box1  = tf.reshape(box1,(1,4))
+	box2  = tf.reshape(box2,(1,4))
 
-	swap      = swap_xy(box)
-	xywh_box = convert_to_xywh(box)
-	xy_corner = convert_to_corner(xywh_box)
-	print(swap)
-	print(xywh_box)
-	print(xy_corner)
+	swap1      = swap_xy(box1)
+	xywh_box1  = convert_to_xywh(box1)
+	xy_corner1 = convert_to_corner(xywh_box1)
+	print(swap1)
+	print(xywh_box1)
+	print(xy_corner1)
+
+
+	#xywh_box1 = tf.expand_dims(xywh_box1, axis = 0)
+
+	xywh_box2 = convert_to_xywh(box2)
+	iou  = compute_iou(xywh_box1, xywh_box2)
+	print(iou)
